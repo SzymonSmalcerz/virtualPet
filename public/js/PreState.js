a5055fe18357280460a836a94a90bf0141dc3045
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2018-06-01 22:27:52 +0200

    small fix

--- public/js/PreState.js
+++ public/js/PreState.js
@@ -1,17 +1,16 @@
 let PreState = {
   init : function (){
+    this.game.stage.backgroundColor = "#fff";
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
     this.scale.refresh();
-
   },
   preload(){
     this.load.image("progressBar","assets/progresBar.png");
     this.load.spritesheet("monkey", "assets/monkey_spritesheet.png",180,180,5);
   },
   create(){
-    this.game.stage.backgroundColor = "#fff";
     this.game.state.start("LoadState");
   }
 }
